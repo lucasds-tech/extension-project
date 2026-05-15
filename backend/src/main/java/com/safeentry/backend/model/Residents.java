d3d0e9db1a1381d04f2b71bad81c89d04cb2ad8f
lucasds-tech <lucas.dasilva09@hotmail.com>
2026-05-15 19:38:54 -0300

    feat: add validation annotations to Residents entity fields and include server error stacktrace configuration

--- backend/src/main/java/com/safeentry/backend/model/Residents.java
+++ backend/src/main/java/com/safeentry/backend/model/Residents.java
@@ -2,11 +2,13 @@ package com.safeentry.backend.model;
 
 import jakarta.persistence.*;
 import lombok.*;
-import org.hibernate.annotations.processing.Pattern;;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.NotBlank;
 
 @Data
 @Entity
 @AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "residents")
 public class Residents {
 
@@ -14,12 +16,16 @@ public class Residents {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "The name is required.")
     @Column(name = "name")
     private String name;
 
+    @NotBlank(message = "The lastName is required.")
     @Column(name = "lastName")
     private String lastName;
 
+    @NotBlank(message = "O documento é obrigatório.")
+    @Pattern(regexp = "\\d+", message = "The document must contain only numbers.")
     @Column(name = "document")
     private String document;
 
